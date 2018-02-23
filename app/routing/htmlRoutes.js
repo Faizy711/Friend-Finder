4003003779fad2aa22cbc6088941e6a7790ada2d
Faizy711 <faizan.s711@gmail.com>
2018-02-22 19:30:55 -0600

    logic maybe done still need onclick submit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,16 +2,17 @@
 var path = require("path");
 
 //Routing
-
 module.exports = (app) => {
-//HTML GET
-    app.get("/home", (req, res) => {
+    //HTML GET
+    //Homepage get
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname,"../public/home.html"));
     });
-
+    //Survey get
     app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname,"../public/survey.html"));
     });
+    //default get??
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     })
